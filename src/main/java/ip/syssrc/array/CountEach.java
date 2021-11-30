4fc1eddbb69f6018e3dd44bda6f9dc1d9692c060
MuchtarAdam <muchtaradam2002@gmail.com>
2021-11-30 22:30:32 +0800

    Update CountEach.java

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -1,11 +1,11 @@
 package ip.syssrc.array;
-
+import java.util.Scanner;
 /**
  * CountEach
  *
  * Assignment 3.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211040 - Muchtar Adam <muchtaradam2002@gmail.com>
  *
  */
 
@@ -13,5 +13,20 @@ public class CountEach {
 
     public static void main(String[] args) {
 
+        Scanner t3 = new Scanner(System.in);
+        int n = t3.nextInt();
+        int[] array = new int[10000000];
+        for (int i = 0; i < n; i++) {
+            int var = t3.nextInt();
+            array[var]++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+
+            } else {
+                System.out.println(array[i] + " angka " + i);
+            }
+        }
+        t3.close();
     }
 }
